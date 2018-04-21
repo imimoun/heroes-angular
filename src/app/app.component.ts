fbcea252d56e806879244539e2b8cfbfbc6ba67b
ilanmimoun <ilan.mimoun@sivan-innovation.com>
2018-04-21 23:40:12 +0300

    Test yael.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App!';
+  title = 'Yael, ma femme est la plus belle!';
 }
